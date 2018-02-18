1dcc765d51f35afbb4d1450237eb581a2d7da870
Safwan Morshed <safan25641@gmail.com>
2018-02-18 12:36:46 -0600

    Update setup.py

--- basic_calc/setup.py
+++ basic_calc/setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
-
+import py2exe
 setup(
     name='basic_calc',
     version='0.1',
     packages=['basic_calc',],
     license='MIT License',
     long_description=open('README.txt').read(),
+    console=['_init_.py']
 )
 

61c694eb842150e8c7d2202579568afab52d81f1
Safwan Morshed <safan25641@gmail.com>
2018-02-18 12:00:09 -0600

    Create setup.py

---
+++ basic_calc/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='basic_calc',
+    version='0.1dev',
+    packages=['basic_calc',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
+

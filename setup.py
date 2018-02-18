1c3a80fae947b4528537e1b8468a40780993ea55
Safwan Morshed <safan25641@gmail.com>
2018-02-18 12:09:33 -0600

    Update and rename basic_calc/setup.py to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='basic_calc',
-    version='0.1dev',
+    version='0.1',
     packages=['basic_calc',],
     license='MIT License',
     long_description=open('README.txt').read(),
